a99824cb846d845deaa8365c6b82969bd7357a99
SebRVV <112523636+SebSRVV@users.noreply.github.com>
2025-09-23 14:33:07 -0500

    Inicializar proyecto Spring Boot con Maven Wrapper y configuracion basica

---
+++ src/main/java/com/sebsrvv/app/SupabaseBackendApplication.java
@@ -0,0 +1,12 @@
+// src/main/java/com/sebsrvv/app/SupabaseBackendApplication.java
+package com.sebsrvv.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SupabaseBackendApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SupabaseBackendApplication.class, args);
+    }
+}
---
+++ src/test/java/com/sebsrvv/app/SupabaseBackendApplicationTests.java
@@ -0,0 +1,11 @@
+// src/test/java/com/sebsrvv/app/SupabaseBackendApplicationTests.java
+package com.sebsrvv.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SupabaseBackendApplicationTests {
+    @Test
+    void contextLoads() { }
+}
