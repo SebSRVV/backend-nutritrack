24511ca744e5604082a318801df20921505b9c41
luisssssfc <u20201a626@upc.edu.pe>
2025-10-01 13:01:38 -0500

    avance

---
+++ src/main/java/com/sebsrvv/app/modules/meals/domain/MealCategory.java
@@ -0,0 +1,10 @@
+package com.sebsrvv.app.modules.meals.domain;
+
+import lombok.Data;
+
+@Data
+public class MealCategory {
+    private Integer id;
+    private String name;
+    private String description;
+}
---
+++ src/main/java/com/sebsrvv/app/modules/meals/domain/MealType.java
@@ -0,0 +1,8 @@
+package com.sebsrvv.app.modules.meals.domain;
+
+public enum MealType {
+    BREAKFAST,
+    LUNCH,
+    DINNER,
+    SNACK
+}
