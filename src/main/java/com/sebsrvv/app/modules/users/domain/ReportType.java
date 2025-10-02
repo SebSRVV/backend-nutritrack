fa0dfc5d41ab15b1e881f5311480dc0cd4b4a44f
Sebastian Rojas <Sebrojasw@gmail.com>
2025-10-02 15:27:03 -0500

    feat: agregar interfaz UserReportRepository y eliminar línea innecesaria en ReportType

--- src/main/java/com/sebsrvv/app/modules/users/domain/ReportType.java
+++ src/main/java/com/sebsrvv/app/modules/users/domain/ReportType.java
@@ -2,7 +2,6 @@ package com.sebsrvv.app.modules.users.domain;
 
 public enum ReportType {
     NUTRITION_PDF("nutrition_pdf");
-
     private final String wire;
     ReportType(String wire){ this.wire = wire; }
     public String wire() { return wire; }
