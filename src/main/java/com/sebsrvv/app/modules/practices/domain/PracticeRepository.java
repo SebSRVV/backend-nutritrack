d4e6a8b3f09ae38d912fa49bd49bfa9a856f00b3
Homura Supremacy <136258763+Andres0542@users.noreply.github.com>
2025-10-26 19:51:09 -0500

    Se anaden APIs de editar, crear y borrar practicas, totalmente funcionales con el Supabase.

--- src/main/java/com/sebsrvv/app/modules/practices/domain/PracticeRepository.java
+++
@@ -1,8 +0,0 @@
-package com.sebsrvv.app.modules.practices.domain;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.util.UUID;
-
-public interface PracticeRepository extends JpaRepository<Practice, UUID> {
-}
