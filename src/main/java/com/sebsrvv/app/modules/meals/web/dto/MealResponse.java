5754f5373321cafdf58840d70daf4c2a68bbc99f
luisssssfc <u20201a626@upc.edu.pe>
2025-11-12 15:05:46 -0500

    feat: actualizar la clase MealResponse para incluir nuevos campos y cambiar el tipo de fecha

--- src/main/java/com/sebsrvv/app/modules/meals/web/dto/MealResponse.java
+++ src/main/java/com/sebsrvv/app/modules/meals/web/dto/MealResponse.java
@@ -3,18 +3,18 @@ package com.sebsrvv.app.modules.meals.web.dto;
 import com.sebsrvv.app.modules.meals.domain.MealType;
 import lombok.Data;
 
-import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
 public class MealResponse {
-    private UUID id;
-    private UUID userId;
-    private String name;
+
+    private UUID id; // opcional, si tu entidad tiene un identificador
     private MealType mealType;
-    private Double calories;
-    private String note;
-    private LocalDate loggedAt;
-    private Integer categoryId;
-    private String categoryName;
+    private String description;
+    private double calories;
+    private double protein_g;
+    private double carbs_g;
+    private double fat_g;
+    private LocalDateTime loggedAt;
 }
