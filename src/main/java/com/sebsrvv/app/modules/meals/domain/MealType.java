684fe38218ce576d50e4066f908ea8e66684ac22
luisssssfc <u20201a626@upc.edu.pe>
2025-11-16 20:33:03 -0500

    feat: agregartipo de comida en enumeración MealType

--- src/main/java/com/sebsrvv/app/modules/meals/domain/MealType.java
+++ src/main/java/com/sebsrvv/app/modules/meals/domain/MealType.java
@@ -1,22 +1,9 @@
 package com.sebsrvv.app.modules.meals.domain;
 
-import com.fasterxml.jackson.annotation.JsonCreator;
-import com.fasterxml.jackson.annotation.JsonValue;
-
 public enum MealType {
     BREAKFAST,
     LUNCH,
     DINNER,
-    SNACK;
-
-    @JsonCreator
-    public static MealType fromString(String key) {
-        if (key == null) return null;
-        return MealType.valueOf(key.trim().toUpperCase());
-    }
-
-    @JsonValue
-    public String toValue() {
-        return this.name();
-    }
+    SNACK,
+    OTHER
 }
