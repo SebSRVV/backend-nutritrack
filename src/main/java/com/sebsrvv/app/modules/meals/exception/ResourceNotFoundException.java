50602e89a35714a55ebab59cc45ded2fb5a1dfbb
SebRVV <U202110299@upc.edu.pe>
2025-11-16 20:24:59 -0500

    feat: limpieza del api

--- src/main/java/com/sebsrvv/app/modules/meals/exception/ResourceNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.sebsrvv.app.modules.meals.exception;
-//Se lanza cuando algo no existe en la base de datos (por ejemplo, un meal no encontrado).
-public class ResourceNotFoundException extends RuntimeException {
-    public ResourceNotFoundException(String message) {
-        super(message);
-    }
-}
-
--- src/main/java/com/sebsrvv/app/modules/meals/exception/UnauthorizedException.java
+++
@@ -1,9 +0,0 @@
-//Se lanza si el usuario no tiene permisos o el token de Supabase no es válido.
-
-package com.sebsrvv.app.modules.meals.exception;
-
-public class UnauthorizedException extends RuntimeException {
-    public UnauthorizedException(String message) {
-        super(message);
-    }
-}
