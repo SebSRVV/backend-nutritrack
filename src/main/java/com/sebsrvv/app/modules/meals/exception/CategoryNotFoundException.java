3dacb482fbd561115aa0dc58121afe06da99fc25
SebRVV <U202110299@upc.edu.pe>
2025-11-16 20:26:39 -0500

    feat: limpieza del api

--- src/main/java/com/sebsrvv/app/modules/meals/exception/CategoryNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.sebsrvv.app.modules.meals.exception;
-
-public class CategoryNotFoundException extends RuntimeException {
-    public CategoryNotFoundException(String message) {
-        super(message);
-    }
-}
--- src/main/java/com/sebsrvv/app/modules/meals/exception/InvalidMealDataException.java
+++
@@ -1,7 +0,0 @@
-package com.sebsrvv.app.modules.meals.exception;
-
-public class InvalidMealDataException extends RuntimeException {
-    public InvalidMealDataException(String message) {
-        super(message);
-    }
-}
